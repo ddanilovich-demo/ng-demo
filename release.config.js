9da9e066b23a2e0d14afe63fc5347257042abbe5
ddanilovich <9818197+ddanilovich@users.noreply.github.com>
2021-10-26 15:59:40 +0500

    fix: add `semantic-release` action
    Closes #9

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle: '# Changelog',
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/github',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md'],
+        message: 'chore: update `CHANGELOG.md` [skip ci]',
+      },
+    ],
+  ],
+};
